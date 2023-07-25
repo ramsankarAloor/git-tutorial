52da0da5b6082d8829f0b3175af70fdba8efe04d
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-07-25 16:41:08 +0530

    Delete data from local storage.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let form = document.getElementById('my-form')
 
 form.addEventListener('submit', doThis)
 
+
 function doThis(e){
   e.preventDefault()
   let username = document.getElementById('name').value 
@@ -15,18 +16,30 @@ function doThis(e){
     'phone' : phone
   })
 
-  let toBePrinted = username+'-'+emailId+'-'+phone
+  let toBePrinted = username+' - '+emailId+' - '+phone
 
   let textInside = document.createTextNode(toBePrinted)
+  let deleteBtn = document.createElement('button')
+  deleteBtn.textContent = "Delete"
+  deleteBtn.className = 'btnDelete'
   let li = document.createElement('li')
-  li.classname = "item"
   li.appendChild(textInside)
+  li.appendChild(deleteBtn)
   
   let ul = document.getElementById('items')
-
+  
   ul.appendChild(li)
-
+  
   localStorage.setItem(emailId, myObj_serialised)
+
+  deleteBtn.addEventListener('click', deleteEle)
+
+  function deleteEle(e){
+    let liCurrent = e.target.parentElement
+    let ul = document.getElementById('items')
+    localStorage.removeItem(emailId)
+    ul.removeChild(liCurrent)
+  }
 
   document.getElementById('name').value = ''
   document.getElementById('email').value = ''
