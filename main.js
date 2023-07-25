56bfd756bfe4cab0e3442c13a28daf2d0a867704
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-07-25 15:56:05 +0530

    Let's scale the app to more users.

--- main.js
+++ main.js
@@ -7,16 +7,30 @@ function doThis(e){
   e.preventDefault()
   let username = document.getElementById('name').value 
   let emailId = document.getElementById('email').value 
+  let phone = document.getElementById('phone').value 
 
   let myObj_serialised = JSON.stringify({
     'name' : username,
-    'email': emailId
+    'email': emailId,
+    'phone' : phone
   })
 
-  localStorage.setItem('my_obj', myObj_serialised)
+  let toBePrinted = username+'-'+emailId+'-'+phone
+
+  let textInside = document.createTextNode(toBePrinted)
+  let li = document.createElement('li')
+  li.classname = "item"
+  li.appendChild(textInside)
+  
+  let ul = document.getElementById('items')
+
+  ul.appendChild(li)
+
+  localStorage.setItem(emailId, myObj_serialised)
 
   document.getElementById('name').value = ''
   document.getElementById('email').value = ''
+  document.getElementById('phone').value = ''
 
 
 }
