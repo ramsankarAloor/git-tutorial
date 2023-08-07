21f8ed6f8c734c1ab07bc7a0e070f1d95532f490
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-08-07 12:10:22 +0530

    Deleting the Appointments.

--- main.js
+++ main.js
@@ -2,6 +2,46 @@ let form = document.getElementById("my-form");
 
 form.addEventListener("submit", doThis);
 
+function addMember(memberObj) {
+  let toBePrinted =
+    memberObj.name + " - " + memberObj.email + " - " + memberObj.phone;
+
+  let textInside = document.createTextNode(toBePrinted);
+  let deleteBtn = document.createElement("button");
+  let editBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  editBtn.textContent = "Edit";
+  deleteBtn.className = "btnDelete";
+  editBtn.className = "btnDelete";
+  let li = document.createElement("li");
+  li.appendChild(textInside);
+  li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+
+  let ul = document.getElementById("items");
+
+  ul.appendChild(li);
+
+  deleteBtn.addEventListener("click", deleteEle);
+
+  function deleteEle(e) {
+    let liCurrent = e.target.parentElement;
+    let ul = document.getElementById("items");
+    ul.removeChild(liCurrent);
+    axios.delete(`https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData/${memberObj._id}`)
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  // editBtn.addEventListener("click", editEle);
+}
+
+axios.get("https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData")
+.then(res=>{
+  for(let i=0; i<res.data.length; i++){
+    addMember(res.data[i])
+  }
+}).catch(err=>console.log(err))
 
 function doThis(e) {
   e.preventDefault();
@@ -15,61 +55,18 @@ function doThis(e) {
     phone: phone,
   };
 
-  let myObj_serialised = JSON.stringify({
-    name: username,
-    email: emailId,
-    phone: phone,
-  });
-
-  axios
-    .post(
-      "https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData",
-      obj
-    )
-    .then(() => {
-      axios
-        .get(
-          "https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData"
-        )
-        .then((resp) => {
-          console.log(resp);
-          for (let i = 0; i < resp.data.length; i++) {
-            let toBePrinted = resp.data[i].name + " - " + resp.data[i].email + " - " + resp.data[i].phone;
-
-            let textInside = document.createTextNode(toBePrinted);
-            let deleteBtn = document.createElement("button");
-            let editBtn = document.createElement("button");
-            deleteBtn.textContent = "Delete";
-            editBtn.textContent = "Edit";
-            deleteBtn.className = "btnDelete";
-            editBtn.className = "btnDelete";
-            let li = document.createElement("li");
-            li.appendChild(textInside);
-            li.appendChild(deleteBtn);
-            li.appendChild(editBtn);
-
-            let ul = document.getElementById("items");
-
-            ul.appendChild(li);
-
-            deleteBtn.addEventListener("click", deleteEle);
-            editBtn.addEventListener("click", editEle);
-          }
-        })
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
+  axios.post("https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData", obj)
+  .then((res)=>{
+    console.log(res)
+    addMember(res.data)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
 
   // localStorage.setItem(emailId, myObj_serialised)
 
   
-
-  function deleteEle(e) {
-    let liCurrent = e.target.parentElement;
-    let ul = document.getElementById("items");
-    localStorage.removeItem(emailId);
-    ul.removeChild(liCurrent);
-  }
 
   function editEle(e) {
     let liCurrent = e.target.parentElement;
