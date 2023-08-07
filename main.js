821668d7b8600f7599689355277b3aa6612b2942
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-08-07 10:42:56 +0530

    Get the saved User Details from crudcrud.

--- main.js
+++ main.js
@@ -1,77 +1,88 @@
+let form = document.getElementById("my-form");
 
-let form = document.getElementById('my-form')
-
-form.addEventListener('submit', doThis)
+form.addEventListener("submit", doThis);
 
 
-function doThis(e){
-  e.preventDefault()
-  let username = document.getElementById('name').value 
-  let emailId = document.getElementById('email').value 
-  let phone = document.getElementById('phone').value 
+function doThis(e) {
+  e.preventDefault();
+  let username = document.getElementById("name").value;
+  let emailId = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
 
   let obj = {
-    'name' : username,
-    'email': emailId,
-    'phone' : phone
-  }
+    name: username,
+    email: emailId,
+    phone: phone,
+  };
 
   let myObj_serialised = JSON.stringify({
-    'name' : username,
-    'email': emailId,
-    'phone' : phone
-  })
+    name: username,
+    email: emailId,
+    phone: phone,
+  });
 
-  let toBePrinted = username+' - '+emailId+' - '+phone
+  axios
+    .post(
+      "https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData",
+      obj
+    )
+    .then(() => {
+      axios
+        .get(
+          "https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData"
+        )
+        .then((resp) => {
+          console.log(resp);
+          for (let i = 0; i < resp.data.length; i++) {
+            let toBePrinted = resp.data[i].name + " - " + resp.data[i].email + " - " + resp.data[i].phone;
 
-  let textInside = document.createTextNode(toBePrinted)
-  let deleteBtn = document.createElement('button')
-  let editBtn = document.createElement('button')
-  deleteBtn.textContent = "Delete"
-  editBtn.textContent = 'Edit'
-  deleteBtn.className = 'btnDelete'
-  editBtn.className = 'btnDelete'
-  let li = document.createElement('li')
-  li.appendChild(textInside)
-  li.appendChild(deleteBtn)
-  li.appendChild(editBtn)
-  
-  let ul = document.getElementById('items')
-  
-  ul.appendChild(li)
+            let textInside = document.createTextNode(toBePrinted);
+            let deleteBtn = document.createElement("button");
+            let editBtn = document.createElement("button");
+            deleteBtn.textContent = "Delete";
+            editBtn.textContent = "Edit";
+            deleteBtn.className = "btnDelete";
+            editBtn.className = "btnDelete";
+            let li = document.createElement("li");
+            li.appendChild(textInside);
+            li.appendChild(deleteBtn);
+            li.appendChild(editBtn);
 
-  axios.post("https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData", obj)
-  .then(resp=>console.log(resp))
-  .catch(err=>console.log(err))
-  
+            let ul = document.getElementById("items");
+
+            ul.appendChild(li);
+
+            deleteBtn.addEventListener("click", deleteEle);
+            editBtn.addEventListener("click", editEle);
+          }
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+
   // localStorage.setItem(emailId, myObj_serialised)
 
-  deleteBtn.addEventListener('click', deleteEle)
-  editBtn.addEventListener('click', editEle)
+  
 
-  function deleteEle(e){
-    let liCurrent = e.target.parentElement
-    let ul = document.getElementById('items')
-    localStorage.removeItem(emailId)
-    ul.removeChild(liCurrent)
+  function deleteEle(e) {
+    let liCurrent = e.target.parentElement;
+    let ul = document.getElementById("items");
+    localStorage.removeItem(emailId);
+    ul.removeChild(liCurrent);
   }
 
-  function editEle(e){
-    let liCurrent = e.target.parentElement
-    let ul = document.getElementById('items')
-    localStorage.removeItem(emailId)
-    ul.removeChild(liCurrent)
+  function editEle(e) {
+    let liCurrent = e.target.parentElement;
+    let ul = document.getElementById("items");
+    localStorage.removeItem(emailId);
+    ul.removeChild(liCurrent);
 
-    document.getElementById('name').value = username
-    document.getElementById('email').value = emailId
-    document.getElementById('phone').value = phone
-
-
+    document.getElementById("name").value = username;
+    document.getElementById("email").value = emailId;
+    document.getElementById("phone").value = phone;
   }
 
-  document.getElementById('name').value = ''
-  document.getElementById('email').value = ''
-  document.getElementById('phone').value = ''
-
-
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
 }
