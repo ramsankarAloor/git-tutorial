1c371906929dde179af512210369d2f2db371985
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-08-07 16:07:59 +0530

    Test Question - 1

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 let form = document.getElementById("my-form");
-
+let sum = 0
 form.addEventListener("submit", doThis);
-let edit = 0;
-let edit_id 
+
 function addMember(memberObj) {
   let toBePrinted =
-    memberObj.name + " - " + memberObj.email + " - " + memberObj.phone;
+    memberObj.price + " - " + memberObj.product + '  ';
 
   let textInside = document.createTextNode(toBePrinted);
   let deleteBtn = document.createElement("button");
-  let editBtn = document.createElement("button");
-  deleteBtn.textContent = "Delete";
-  editBtn.textContent = "Edit";
-  deleteBtn.className = "btnDelete";
-  editBtn.className = "btnDelete";
+  deleteBtn.textContent = "Delete Product";
   let li = document.createElement("li");
   li.appendChild(textInside);
   li.appendChild(deleteBtn);
-  li.appendChild(editBtn);
 
   let ul = document.getElementById("items");
 
@@ -31,91 +25,60 @@ function addMember(memberObj) {
     ul.removeChild(liCurrent);
     axios
       .delete(
-        `https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData/${memberObj._id}`
-      )
+        `https://crudcrud.com/api/7793b69ddb824f17a597f48fbad7dfb9/productDetails/${memberObj._id}`
+      ).then(()=>{
+        sum -= Number(memberObj.price)
+        displaySum()
+      })
       .catch((err) => {
         console.log(err);
       });
   }
 
-  editBtn.addEventListener("click", editEle);
 
-  function editEle(e) {
-    edit = 1
-    let liCurrent = e.target.parentElement;
-    let ul = document.getElementById("items");
-    // localStorage.removeItem(emailId);
-    ul.removeChild(liCurrent);
-
-    document.getElementById("name").value = memberObj.name;
-    document.getElementById("email").value = memberObj.email;
-    document.getElementById("phone").value = memberObj.phone;
-
-    edit_id = memberObj._id
-    // axios
-    //   .put(
-    //     `https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData/${memberObj._id}`,
-    //     updated
-    //   )
-    //   .then((res) => {
-    //     addMember(res.data);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  }
+  
 }
 
-axios
-  .get(
-    "https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData"
-  )
-  .then((res) => {
-    for (let i = 0; i < res.data.length; i++) {
-      addMember(res.data[i]);
-    }
-  })
-  .catch((err) => console.log(err));
+axios.get("https://crudcrud.com/api/7793b69ddb824f17a597f48fbad7dfb9/productDetails")
+.then((res)=>{
+  for(let i=0; i<res.data.length; i++){
+    sum += Number(res.data[i].price)
+    addMember(res.data[i])
+  }
+  displaySum()
+})
+.catch((err)=>{
+  console.log(err)
+})
+
+
 
 function doThis(e) {
   e.preventDefault();
-  let username = document.getElementById("name").value;
-  let emailId = document.getElementById("email").value;
-  let phone = document.getElementById("phone").value;
+  
+  let price = document.getElementById("price").value
+  let product = document.getElementById("product").value
 
   let obj = {
-    name: username,
-    email: emailId,
-    phone: phone,
+    price : price,
+    product : product
   };
-  if (edit === 0) {
-    axios
-      .post(
-        "https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData",
-        obj
-      )
-      .then((res) => {
-        addMember(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }else if(edit === 1){
-    axios
-      .put(
-        `https://crudcrud.com/api/3c0a338408744ded89f9e06f14ba0193/appointmentData/${edit_id}`,
-        obj
-      )
-      .then(() => {
-        addMember(obj);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-      edit = 0
-  }
 
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("phone").value = "";
+  axios.post("https://crudcrud.com/api/7793b69ddb824f17a597f48fbad7dfb9/productDetails", obj)
+  .then((res)=>{
+    addMember(res.data)
+    sum += Number(res.data.price)
+    displaySum()
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+
+  document.getElementById("price").value = "";
+  document.getElementById("product").value = "";
 }
+
+function displaySum(){
+  document.getElementById("suminput").value = sum
+}
+
